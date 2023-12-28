b2550d984d480d373db8c97cb3a96c44c5b282ea
HadRad500 <hadignasr@gmail.com>
2023-12-28 15:10:15 -0800

    userRegistration form creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from "./app.component";
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 @NgModule({
     declarations: [
         AppComponent
@@ -12,7 +21,15 @@ import { AppComponent } from "./app.component";
     imports: [
         BrowserModule,
         HttpClientModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatDialogModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatSnackBarModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
